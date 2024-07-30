4de4eb21ff7347638cc725ecee46e891c619a29b
Lucas-Choi-17 <csissis@naver.com>
2024-07-30 21:45:20 +0900

    feat : 지도 클릭 시 화면에 핀 표시 기능

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,3 +1,8 @@
+/*
+ * 참고자료 - NAVER CLOUD PLATFORM - NAVER Map's Enterprise API 지도 예제
+ * 링크 : https://navermaps.github.io/maps.js.ncp/docs/tutorial-digest.example.html
+ */
+
 // 지도 api를 불러오기 전에 코드들이 수행되면 ready 조건 밖의 코드들이 수행이 안되는 현상이 있음
 // 해결법 유무는 찾아봐야함
 $(document).ready(function () {
@@ -24,5 +29,61 @@ $(document).ready(function () {
         $('#center tr:nth-child(2) th')[0].textContent = map.center._lat;
         $('#center tr:nth-child(2) th')[1].textContent = map.center._lng;
     });
+
+    // 핀 표시된 장소의 위치정보를 모두 저장
+    var markerList = [];
+
+    // 지도를 클릭할 경우, 해당 위치에 핀이 표시되고 이는 저장되어 누적된다.
+    naver.maps.Event.addListener(map, 'click', function (e) {
+        // 핀 표시
+        var marker = new naver.maps.Marker({
+            position: e.coord,
+            map: map
+        });
+
+        // 표시된 핀의 위도와 경도를 누적해서 보여준다.
+        $('#pined_list tbody')[0].append(document.createElement('tr'));
+        $('#pined_list tbody tr:last-child')[0].append(document.createElement('th'));
+        $('#pined_list tbody tr:last-child th:last-child')[0].append(e.coord._lat);
+        $('#pined_list tbody tr:last-child')[0].append(document.createElement('th'));
+        $('#pined_list tbody tr:last-child th:last-child')[0].append(e.coord._lng);
+
+        // 추가된 핀의 정보를 리스트에 넣어준다.
+        markerList.push(marker);
+
+        console.log(markerList);
+    });
+
+    // 핀의 옵션 지정
+    // 핀의 클릭 영역을 지정
+    // var markerOptions = {
+    //     position: position,
+    //     map: map,
+    //     shape: {
+    //         coords: [11, 0, 9, 0, 6, 1, 4, 2, 2, 4,
+    //             0, 8, 0, 12, 1, 14, 2, 16, 5, 19,
+    //             5, 20, 6, 23, 8, 26, 9, 30, 9, 34,
+    //             13, 34, 13, 30, 14, 26, 16, 23, 17, 20,
+    //             17, 19, 20, 16, 21, 14, 22, 12, 22, 12,
+    //             22, 8, 20, 4, 18, 2, 16, 1, 13, 0],
+    //         type: 'poly'
+    //     }
+    // };
+
+    // 모든 핀 제거 및 리스트 초기화
+    $('#erase_pinned_list')[0].addEventListener('click', function () {
+
+        for (let i = 0; i < markerList.length; i++) {
+            markerList[i].setMap(null);
+        }
+        markerList = [];
+
+        $('#pined_list tbody')[0].innerHTML = "";
+        $('#pined_list tbody')[0].append(document.createElement('tr'));
+        $('#pined_list tbody tr:last-child')[0].append(document.createElement('th'));
+        $('#pined_list tbody tr:last-child th:last-child')[0].append("위도");
+        $('#pined_list tbody tr:last-child')[0].append(document.createElement('th'));
+        $('#pined_list tbody tr:last-child th:last-child')[0].append("경도");
+    })
 });
 
