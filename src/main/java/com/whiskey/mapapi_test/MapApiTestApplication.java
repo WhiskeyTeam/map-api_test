7177b0a6bfa8de49e7eb8a5b32afdbbf8060442c
Lucas-Choi-17 <csissis@naver.com>
2024-07-30 14:31:48 +0900

    feat : initial map-api_test project

---
+++ src/main/java/com/whiskey/mapapi_test/MapApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.whiskey.mapapi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapApiTestApplication.class, args);
+    }
+
+}
