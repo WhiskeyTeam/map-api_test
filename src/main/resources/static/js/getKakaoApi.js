39b27bd6c00f4233b3ffc72efee880fda35c5e12
Lucas-Choi-17 <csissis@naver.com>
2024-08-03 17:51:34 +0900

    feat : kakao map api - 카카오맵 api key 호출 기능 구현

---
+++ src/main/resources/static/js/getKakaoApi.js
@@ -0,0 +1,28 @@
+function getConfig() {
+
+    var result;
+
+    $.ajax({
+        url: '/kakao-api-key',
+        async: false,
+        type: 'get',
+        success: function (data) {
+            result = data;
+        },
+        error: function () {
+            console.log("cannot get key from server.");
+        }
+    })
+    return result;
+}
+
+function setApiConfig(key) {
+    return new Promise(() => {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=' + key + '&autoload=false';
+        document.head.prepend(script);
+    });
+}
+
+setApiConfig(getConfig());
