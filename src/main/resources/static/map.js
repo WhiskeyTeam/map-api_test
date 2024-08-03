42ed2d1742b11962ef21134bd10a9cc951f0a005
Lucas-Choi-17 <csissis@naver.com>
2024-08-03 17:48:12 +0900

    comment : naver map api 기능 설명 주석 추가

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -7,33 +7,34 @@
 // 해결법 유무는 찾아봐야함
 $(document).ready(function () {
 
-    // 지도 표시 기능
+    /* 지도 표시 기능 */
     var map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng(37.5112, 127.0981), // 잠실 롯데월드를 중심으로 하는 지도 -> 설정된 좌표를 중심으로 지도 렌더링
         zoom: 15 // 확대 정도
     });
 
-    // 지도상에 핀을 찍는 기능
-    var marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(37.5112, 127.0981), // 위에서 설정한 바와 같이 처음 렌더링되는 지도의 중앙인 잠실 롯데월드에 핀을 찍음
-        map: map
-    });
+    /* 지도상에 핀을 찍는 기능 */
+    // var marker = new naver.maps.Marker({
+    //     position: new naver.maps.LatLng(37.5112, 127.0981), // 위에서 설정한 바와 같이 처음 렌더링되는 지도의 중앙인 잠실 롯데월드에 핀을 찍음
+    //     map: map
+    // });
 
-    // map.center는 위도와 경도 값을 가지고 있음 -> 위도 = _lat, 경도 = _lng
+    /* map.center는 위도와 경도 값을 가지고 있음 -> 위도 = _lat, 경도 = _lng */
     document.getElementById('checkBtn').addEventListener('click', function () {
         alert("위도 : " + map.center._lat + "  " + "경도 : " + map.center._lng);
     });
 
-    // 지도를 이동할 때마다 지도 중앙의 좌표를 표시하는 기능
+    /* 지도를 이동할 때마다 지도 중앙의 좌표를 표시하는 기능 */
     naver.maps.Event.addListener(map, 'drag', function (e) {
         $('#center tr:nth-child(2) th')[0].textContent = map.center._lat;
         $('#center tr:nth-child(2) th')[1].textContent = map.center._lng;
     });
 
-    // 핀 표시된 장소의 위치정보를 모두 저장
+
+    /* 핀 표시된 장소의 위치정보를 모두 저장 */
     var markerList = [];
 
-    // 지도를 클릭할 경우, 해당 위치에 핀이 표시되고 이는 저장되어 누적된다.
+    /* 지도를 클릭할 경우, 해당 위치에 핀이 표시되고 이는 저장되어 누적된다. */
     naver.maps.Event.addListener(map, 'click', function (e) {
         // 핀 표시
         var marker = new naver.maps.Marker({
@@ -54,8 +55,8 @@ $(document).ready(function () {
         console.log(markerList);
     });
 
-    // 핀의 옵션 지정
-    // 핀의 클릭 영역을 지정
+    /* 핀의 옵션 지정 */
+    /* 핀의 클릭 영역을 지정 - 미완료 */
     // var markerOptions = {
     //     position: position,
     //     map: map,
@@ -70,7 +71,7 @@ $(document).ready(function () {
     //     }
     // };
 
-    // 모든 핀 제거 및 리스트 초기화
+    /* 모든 핀 제거 및 리스트 초기화 */
     $('#erase_pinned_list')[0].addEventListener('click', function () {
 
         for (let i = 0; i < markerList.length; i++) {
